4ed0118ce03f0b8c7926b4421259efd50679e265
asmaagoma <asmaagadallaah@gmail.com>
2024-08-07 04:43:56 +0300

    :"(

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ basic auth module
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,4 +26,21 @@ class BasicAuth(Auth):
         if len(authorization_header) > 6:
             return authorization_header[6:]
         else:
+            return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+        decode_base64_authorization_header method
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except Exception:
             return None
