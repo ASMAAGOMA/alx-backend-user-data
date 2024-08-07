71104240921fb4fc981ee8a4ca8c96263838119a
asmaagoma <asmaagadallaah@gmail.com>
2024-08-07 04:13:59 +0300

    :"(

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,18 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extract_base64_authorization_header method
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        if len(authorization_header) > 6:
+            return authorization_header[6:]
+        else:
+            return None
